3ce6cf2acb584dd2b182e6320f604c171d91eb10
geroomercante <geronimo.mercante@creativecoefficient.net>
2023-08-30 17:03:28 -0300

    QRKVET08 - Actualizacion agosto

--- src/components/index.js
+++ src/components/index.js
@@ -3,39 +3,13 @@ import img1 from "./ofertas-mensuales/01.jpg";
 import img2 from "./ofertas-mensuales/02.jpg";
 import img3 from "./ofertas-mensuales/03.jpg";
 import img4 from "./ofertas-mensuales/04.jpg";
-import img5 from "./ofertas-mensuales/05.jpg";
-import img6 from "./ofertas-mensuales/06.jpg";
-import img7 from "./ofertas-mensuales/07.jpg";
-import img8 from "./ofertas-mensuales/08.jpg";
-import img9 from "./ofertas-mensuales/09.jpg";
-import img10 from "./ofertas-mensuales/10.jpg";
-import img11 from "./ofertas-mensuales/11.jpg";
-import img12 from "./ofertas-mensuales/12.jpg";
-import img13 from "./ofertas-mensuales/13.jpg";
-import img14 from "./ofertas-mensuales/14.jpg";
-import img15 from "./ofertas-mensuales/15.jpg";
-import img16 from "./ofertas-mensuales/16.jpg";
 // Ofertas Semanales
 import image1 from "./ofertas-semanales/01.jpg";
 import image2 from "./ofertas-semanales/02.jpg";
+import image3 from "./ofertas-semanales/03.jpg";
+import image4 from "./ofertas-semanales/04.jpg";
+import image5 from "./ofertas-semanales/05.jpg";
 
-export const OfertasSemanalesImg = [image1, image2];
+export const OfertasSemanalesImg = [image1, image2, image3, image4, image5];
 
-export const OfertasMensualesImg = [
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
-  img13,
-  img14,
-  img15,
-  img16,
-];
+export const OfertasMensualesImg = [img1, img2, img3, img4];
