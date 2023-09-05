f86acc5c32201df0eafe8863a388e0b01a1e6a9e
andypallero1 <andrespallero6@gmail.com>
2023-09-05 15:13:14 -0300

    actualizacion mensual septiembre

--- src/components/index.js
+++ src/components/index.js
@@ -1,32 +1,32 @@
 // Ofertas mensuales
-import img17 from "./ofertas-mensuales/17.jpg";
-import img18 from "./ofertas-mensuales/18.jpg";
-import img19 from "./ofertas-mensuales/19.jpg";
-import img20 from "./ofertas-mensuales/20.jpg";
-import img21 from "./ofertas-mensuales/21.jpg";
-import img22 from "./ofertas-mensuales/22.jpg";
-import img23 from "./ofertas-mensuales/23.jpg";
-import img24 from "./ofertas-mensuales/24.jpg";
-import img25 from "./ofertas-mensuales/25.jpg";
-import img26 from "./ofertas-mensuales/26.jpg";
-import img27 from "./ofertas-mensuales/27.jpg";
-import img28 from "./ofertas-mensuales/28.jpg";
+import img01 from "./ofertas-mensuales/01.jpg";
+import img02 from "./ofertas-mensuales/02.jpg";
+import img03 from "./ofertas-mensuales/03.jpg";
+import img04 from "./ofertas-mensuales/04.jpg";
+import img05 from "./ofertas-mensuales/05.jpg";
+import img06 from "./ofertas-mensuales/06.jpg";
+import img07 from "./ofertas-mensuales/07.jpg";
+import img08 from "./ofertas-mensuales/08.jpg";
+import img09 from "./ofertas-mensuales/09.jpg";
+import img10 from "./ofertas-mensuales/10.jpg";
+import img11 from "./ofertas-mensuales/11.jpg";
+import img12 from "./ofertas-mensuales/12.jpg";
 // Ofertas Semanales
 import image1 from "./ofertas-semanales/01.jpg";
 
 export const OfertasSemanalesImg = [image1];
 
 export const OfertasMensualesImg = [
-  img17,
-  img18,
-  img19,
-  img20,
-  img21,
-  img22,
-  img23,
-  img24,
-  img25,
-  img26,
-  img27,
-  img28,
+  img01,
+  img02,
+  img03,
+  img04,
+  img05,
+  img06,
+  img07,
+  img08,
+  img09,
+  img10,
+  img11,
+  img12,
 ];
