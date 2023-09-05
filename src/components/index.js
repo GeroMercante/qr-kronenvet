bff0d315a84915a1d400ae7d8ca085dca7fb60b4
andypallero1 <andrespallero6@gmail.com>
2023-09-05 15:04:52 -0300

    qr septiembre

--- src/components/index.js
+++ src/components/index.js
@@ -1,41 +1,32 @@
 // Ofertas mensuales
-import img1 from "./ofertas-mensuales/01.jpg";
-import img2 from "./ofertas-mensuales/02.jpg";
-import img3 from "./ofertas-mensuales/03.jpg";
-import img4 from "./ofertas-mensuales/04.jpg";
-import img5 from "./ofertas-mensuales/05.jpg";
-import img6 from "./ofertas-mensuales/06.jpg";
-import img7 from "./ofertas-mensuales/07.jpg";
-import img8 from "./ofertas-mensuales/08.jpg";
-import img9 from "./ofertas-mensuales/09.jpg";
-import img10 from "./ofertas-mensuales/10.jpg";
-import img11 from "./ofertas-mensuales/11.jpg";
-import img12 from "./ofertas-mensuales/12.jpg";
-import img13 from "./ofertas-mensuales/13.jpg";
-import img14 from "./ofertas-mensuales/14.jpg";
-import img15 from "./ofertas-mensuales/15.jpg";
-import img16 from "./ofertas-mensuales/16.jpg";
+import img17 from "./ofertas-mensuales/17.jpg";
+import img18 from "./ofertas-mensuales/18.jpg";
+import img19 from "./ofertas-mensuales/19.jpg";
+import img20 from "./ofertas-mensuales/20.jpg";
+import img21 from "./ofertas-mensuales/21.jpg";
+import img22 from "./ofertas-mensuales/22.jpg";
+import img23 from "./ofertas-mensuales/23.jpg";
+import img24 from "./ofertas-mensuales/24.jpg";
+import img25 from "./ofertas-mensuales/25.jpg";
+import img26 from "./ofertas-mensuales/26.jpg";
+import img27 from "./ofertas-mensuales/27.jpg";
+import img28 from "./ofertas-mensuales/28.jpg";
 // Ofertas Semanales
 import image1 from "./ofertas-semanales/01.jpg";
-import image2 from "./ofertas-semanales/02.jpg";
 
-export const OfertasSemanalesImg = [image1, image2];
+export const OfertasSemanalesImg = [image1];
 
 export const OfertasMensualesImg = [
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
-  img13,
-  img14,
-  img15,
-  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+  img27,
+  img28,
 ];
