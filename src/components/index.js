20e1a1d8dbdb3cbee00c9c29aee27a4e38d70609
Andypallero1 <andrespallero6@gmail.com>
2024-01-11 17:39:50 -0300

    cambios os

--- src/components/index.js
+++ src/components/index.js
@@ -20,6 +20,10 @@ import img14 from "./ofertas-mensuales/14.jpg";
 import image1 from "./ofertas-semanales/01.jpg";
 import image2 from "./ofertas-semanales/02.jpg";
 import image3 from "./ofertas-semanales/03.jpg";
+import image4 from "./ofertas-semanales/04.jpg";
+import image5 from "./ofertas-semanales/05.jpg";
+import image6 from "./ofertas-semanales/06.jpg";
+import image7 from "./ofertas-semanales/07.jpg";
 
 
 
